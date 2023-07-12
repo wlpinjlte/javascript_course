e219ef0d9d4629dfec2f0c53f552cc534c4d0a07
Mateusz Waga-laptop <wplinjlte@gmail,com>
2023-07-12 23:00:25 +0200

    lab2 homework done

---
+++ lab2/homework/lab1-classwork/logoScript.js
@@ -0,0 +1,48 @@
+var canvas = document.querySelector('canvas');
+console.log(canvas)
+let ctx = canvas.getContext('2d');
+let width=canvas.offsetWidth;
+let height=canvas.offsetHeight;
+console.log(height)        
+// ctx.fillText("Hello World", 10, 50); //Wykreślenie podanego tekstu na płótnie
+ctx.lineWidth=2/75*width;
+ctx.strokeStyle='#6c706e'
+//triangle
+ctx.beginPath()
+ctx.moveTo(1/30*width,1/3*height)
+ctx.lineTo(1/2*width,1/30*height)
+ctx.lineTo(29/30*width,1/3*height)
+ctx.closePath();
+ctx.stroke();
+
+//circle
+ctx.beginPath()
+ctx.arc(1/2*width,5.85/30*height,4/30*width,0,2*Math.PI)
+ctx.closePath();
+ctx.stroke();
+
+//square
+ctx.beginPath()
+ctx.moveTo(2/30*width,1/3*height)
+ctx.lineTo(2/30*width,25/30*height)
+ctx.lineTo(28/30*width,25/30*height)
+ctx.lineTo(28/30*width,1/3*height)
+ctx.closePath();
+ctx.stroke();
+
+//reactangle
+ctx.beginPath()
+ctx.moveTo(1/30*width,25/30*height)
+ctx.lineTo(1/30*width,35/36*height)
+ctx.lineTo(30/31*width,35/36*height)
+ctx.lineTo(30/31*width,25/30*height)
+ctx.closePath();
+ctx.stroke();
+
+//"ABC" text
+ctx.font=`bold ${width/3}px serif`
+ctx.fillText("ABC", 45/300*width, 210/300*height);
+
+//"BANK" text
+ctx.font=`${width/6}px serif`
+ctx.fillText("Bank", 1/3*width, 335/350*height);
